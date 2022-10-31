c9a3fa9616e32a077238df3980a09636583743ad
asol25 <usool.203@gmail.com>
2022-11-01 03:15:02 +0700

    first commit message

---
+++ index.js
@@ -0,0 +1,31 @@
+const data = {
+    "title": "PRO SALE: Get 50% off annual Pro memberships | USE CODE HALLOWEEN22",
+    "fields": {
+        "header": {
+            "logo": "NAME",
+            "nav": [
+                "Home",
+                "Courses",
+                "Contribute",
+                "Download"
+            ],
+            "logo_banner": "./pictures/Moon light logo template.png"
+        },
+        "banner": {
+            "title": "the platform Learning code",
+            "button": "Download"
+        },
+        "courses": {
+            "courses_container": {
+                "title": "A broad selection of courses",
+                "description" :"Choose from 204,000 online video courses with new additions published every month"
+            },
+            "courses_populated": {
+                "title" : "Grow your software development skills with JavaScript",
+                "description": "JavaScript is a text-based computer programming language used to make dynamic web pages. A must-learn for aspiring web developers or programmers, JavaScript can be used for features like image carousels, displaying countdowns and timers, and playing media on a webpage. With JavaScript online classes, you can learn to build interactive web applications, choose the best framework, and work with other programming languages like HTML and CSS."
+            }
+        }
+    }
+}
+
+console.log(data.fields);
---
+++ server/README.md
@@ -0,0 +1,1 @@
+# ecommerce_exe_app
