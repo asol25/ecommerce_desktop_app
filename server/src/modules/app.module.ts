a8a3533eb2cd756d626dd7d2b81debf94841c9b5
asol25 <usool.203@gmail.com>
2022-11-03 00:50:56 +0700

    setup folder

--- server/src/modules/app.module.ts
+++ server/src/modules/app.module.ts
@@ -1,11 +1,9 @@
-import { CatsController } from '../cats.controller';
 import { Module } from '@nestjs/common';
-import { AppController } from '../app.controller';
-import { AppService } from '../app.service';
+import { AuthModule } from './example/bootstap';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CatsController],
-  providers: [AppService],
+  imports: [AuthModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ server/src/modules/example/bootstap/index.ts
@@ -0,0 +1,9 @@
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { Module } from '@nestjs/common';
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AuthModule {}
