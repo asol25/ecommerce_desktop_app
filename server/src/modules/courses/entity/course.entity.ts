afd7cac09d91fb6003fd03287606d598f3cbc1c8
asol25 <usool.203@gmail.com>
2022-11-10 12:59:15 +0700

    add relationship courses to rating

--- server/src/modules/courses/entity/course.entity.ts
+++ server/src/modules/courses/entity/course.entity.ts
@@ -1,6 +1,7 @@
 import { Orders } from "src/modules/orders/entity/orders.entity";
-import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, ManyToOne, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 import { Categorys } from 'src/modules/categorys/entity/categorys.entity';
+import { Rating } from "src/modules/ratings/entity/rating.entity";
 
 @Entity()
 export class Courses {
@@ -15,6 +16,9 @@ export class Courses {
 
     @Column()
     thumbnailUrl: string
+
+    @OneToOne(() => Rating)
+    rating: Rating
 
     @OneToMany(() => Orders, (order) => order.id)
     order: Orders[]
