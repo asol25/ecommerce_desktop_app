e568862daa2390f2e6446013ba13479ac544c9e7
asol25 <usool.203@gmail.com>
2022-11-22 16:59:59 +0700

    cofig PORT SERVER

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 import process from 'process';
 async function bootstrap() {
+  const port = process.env.PORT || 33714
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(33714);
+  await app.listen(port);
 }
 bootstrap();
   
