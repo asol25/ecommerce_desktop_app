62e7b4af57c1ad51b0cba75fe6a23601c5fa772c
asol25 <usool.203@gmail.com>
2022-11-22 16:01:37 +0700

    fix bug Procfile && PORT

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   console.log(`[server]: Server is running at ${process.env.PORT}`);
-  await app.listen(process.env.PORT || 5000);
+  await app.listen(process.env.PORT || 33714);
 }
 bootstrap();
