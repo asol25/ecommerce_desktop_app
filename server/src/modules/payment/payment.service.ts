df80801021f300da7890524ce173775665857c6d
asol25 <usool.203@gmail.com>
2022-11-23 06:53:51 +0700

    check issue check account payment has received

--- server/src/modules/payment/payment.service.ts
+++ server/src/modules/payment/payment.service.ts
@@ -1,5 +1,7 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { catchError } from 'rxjs';
+import { firstValueFrom } from 'rxjs/internal/firstValueFrom';
 
 @Injectable()
 export class PaymentService {
@@ -7,7 +9,18 @@ export class PaymentService {
     private readonly httpService: HttpService
   ) { }
   async getAccountPayment(url) {
-   const isChecked = await this.httpService.get(url);
-   const payload = isChecked.subscribe();
+    const { data } = await firstValueFrom(
+      this.httpService.get(url).pipe(
+        catchError((error) => {
+          console.log(error.response.data);
+          throw 'An error happened!';
+        }),
+      ),
+    );
+    if (data.includes("vnp_TransactionStatus=00")) {
+        console.log("Payment has Received");
+        
+    }
+    return data;
   }
 }
