4eaa51060f39db7906ffbcf5e2c5e2a71b27dcdc
asol25 <usool.203@gmail.com>
2022-11-22 16:54:44 +0700

    fix connect ssl database

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import process from 'process';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  console.log(`[server]: Server is running at ${process.env.PORT}`);
   await app.listen(process.env.PORT || 33714);
 }
 bootstrap();
