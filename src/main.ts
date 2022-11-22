2420af8e536919d2957b5a2e5c88afaeb2ad0bfd
asol25 <usool.203@gmail.com>
2022-11-22 16:04:39 +0700

    fix bug Procfile && PORT

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
-
+import process from 'process';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
