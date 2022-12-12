ef1b3edf922c2c9fceb7a5154a7c7a63fb6cf2e4
asol25 <usool.203@gmail.com>
2022-12-12 10:37:46 +0700

    config env production

--- cms/src/apis/analytic.js
+++ cms/src/apis/analytic.js
@@ -1,17 +1,17 @@
 import axios from "axios"
 
 export const find = async () => {
-    const response = await axios.get(`${process.env.REACT_APP_VERCEL_ENV_API || 'http://localhost:33714'}/analytic`);
+    const response = await axios.get(`${(process.env.REACT_APP_VERCEL_ENV_API || 'http://localhost:33714')}/analytic`);
     return response;
 }
 
 export const getAnalyticFlowNowMonth = async (month) => {
-    const response = await axios.get(`${process.env.REACT_APP_VERCEL_ENV_API || 'http://localhost:33714'}/orders/${month}`);
+    const response = await axios.get(`${(process.env.REACT_APP_VERCEL_ENV_API || 'http://localhost:33714')}/orders/${month}`);
     return response;
 }
 
 export const getAnalyticFlowNowYear = async (year) => {
-    const response = await axios.get(`${process.env.REACT_APP_VERCEL_ENV_API || 'http://localhost:33714'}/orders/total/prices/year/`);
+    const response = await axios.get(`${(process.env.REACT_APP_VERCEL_ENV_API || 'http://localhost:33714')}/orders/total/prices/year/`);
     return response;
 }
 
--- cms/src/apis/dashboard.js
+++ cms/src/apis/dashboard.js
@@ -1,10 +1,10 @@
 export const urls = () => {
     return [
-        `${process.env.REACT_APP_VERCEL_ENV_API || 'http://localhost:33714'}/user/countUsers`,
-        `${process.env.REACT_APP_VERCEL_ENV_API || 'http://localhost:33714'}/courses/totalCourseCount`,
-        `${process.env.REACT_APP_VERCEL_ENV_API || 'http://localhost:33714'}/orders/total/bought`,
-        `${process.env.REACT_APP_VERCEL_ENV_API || 'http://localhost:33714'}/area`,
-        `${process.env.REACT_APP_VERCEL_ENV_API || 'http://localhost:33714'}/country/`,
-        `${process.env.REACT_APP_VERCEL_ENV_API || 'http://localhost:33714'}/orders/total/prices`,
+        `${(process.env.REACT_APP_VERCEL_ENV_API || 'http://localhost:33714')}/user/countUsers`,
+        `${(process.env.REACT_APP_VERCEL_ENV_API || 'http://localhost:33714')}/courses/totalCourseCount`,
+        `${(process.env.REACT_APP_VERCEL_ENV_API || 'http://localhost:33714')}/orders/total/bought`,
+        `${(process.env.REACT_APP_VERCEL_ENV_API || 'http://localhost:33714')}/area`,
+        `${(process.env.REACT_APP_VERCEL_ENV_API || 'http://localhost:33714')}/country/`,
+        `${(process.env.REACT_APP_VERCEL_ENV_API || 'http://localhost:33714')}/orders/total/prices`,
     ]
 }
